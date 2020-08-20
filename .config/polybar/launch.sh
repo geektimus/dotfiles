69286a2d184d0846ee977c3a3730d947e98cee13
Geektimus <alexander.kno+github@gmail.com>
2020-08-19 22:07:38 -0500

    Add polibar, bspmwm and sxhkd config files

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+
+# polybar -c ~/.config/polybar/config.ini main &
+
+# Launch a bar for each monitor
+
+if type "xrandr"; then
+  SCREENS=$(xrandr --query | grep " connected" | wc -l)
+  if [[ $SCREENS != "1" ]]; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      if [[ "$m" == "HDMI-1" ]]; then
+    	  MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+      elif [[ "$m" == "eDP-1" ]]; then
+    	  MONITOR=$m polybar -c ~/.config/polybar/config.ini secondary &
+      fi
+    done
+  else
+    MONITOR="eDP-1" polybar -c ~/.config/polybar/config.ini main &
+  fi
+else
+  MONITOR="eDP-1" polybar -c ~/.config/polybar/config.ini main &
+fi

9d42c2cd31be23d59263b8bdee206ce5e9fd0a84
Geektimus <alexander.kno+github@gmail.com>
2021-02-25 18:40:09 -0500

    Upgrade polybar themes and start using pywal

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,32 +1,100 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-## Add this to your wm startup file.
+dir="$HOME/.config/polybar"
+# themes=(`ls --hide="launch.sh" $dir`)
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-
-# polybar -c ~/.config/polybar/config.ini main &
-
-# Launch a bar for each monitor
-
-if command -v "xrandr" &> /dev/null; then
-  SCREENS=$(xrandr --query | grep " connected" | wc -l)
-  if [[ "$SCREENS" == "2" ]]; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      if [[ "$m" == "HDMI-1" ]]; then
-    	  MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
-      elif [[ "$m" == "eDP-1" ]]; then
-    	  MONITOR=$m polybar -c ~/.config/polybar/config.ini secondary &
-      fi
-    done
+setup_monitors_and_launch() {
+  if command -v "xrandr" &> /dev/null; then
+    SCREENS=$(xrandr --query | grep " connected" | wc -l)
+    if [[ "$SCREENS" == "2" ]]; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        if [[ "$m" == "HDMI-1" ]]; then
+          MONITOR=$m polybar -c "$dir/$style/config.ini" main &
+        elif [[ "$m" == "eDP-1" ]]; then
+          MONITOR=$m polybar -c "$dir/$style/config.ini" secondary &
+        fi
+      done
+    else
+      MONITOR="eDP-1" polybar -c "$dir/$style/config.ini" main &
+    fi
   else
-    MONITOR="eDP-1" polybar -c ~/.config/polybar/config.ini main &
+    MONITOR="eDP-1" polybar -c "$dir/$style/config.ini" main &
   fi
+}
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+		polybar -q top -c "$dir/$style/config.ini" &
+		polybar -q bottom -c "$dir/$style/config.ini" &
+	elif [[ "$style" == "pwidgets" ]]; then
+		bash "$dir"/pwidgets/launch.sh --main
+	else
+		setup_monitors_and_launch	
+	fi
+}
+
+if [[ "$1" == "--material" ]]; then
+	style="material"
+	launch_bar
+
+elif [[ "$1" == "--shades" ]]; then
+	style="shades"
+	launch_bar
+
+elif [[ "$1" == "--hack" ]]; then
+	style="hack"
+	launch_bar
+
+elif [[ "$1" == "--docky" ]]; then
+	style="docky"
+	launch_bar
+
+elif [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+elif [[ "$1" == "--shapes" ]]; then
+	style="shapes"
+	launch_bar
+
+elif [[ "$1" == "--grayblocks" ]]; then
+	style="grayblocks"
+	launch_bar
+
+elif [[ "$1" == "--blocks" ]]; then
+	style="blocks"
+	launch_bar
+
+elif [[ "$1" == "--colorblocks" ]]; then
+	style="colorblocks"
+	launch_bar
+
+elif [[ "$1" == "--forest" ]]; then
+	style="forest"
+	launch_bar
+
+elif [[ "$1" == "--pwidgets" ]]; then
+	style="pwidgets"
+	launch_bar
+
+elif [[ "$1" == "--panels" ]]; then
+	style="panels"
+	launch_bar
+
 else
-  MONITOR="eDP-1" polybar -c ~/.config/polybar/config.ini main &
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--blocks    --colorblocks    --cuts      --docky
+	--forest    --grayblocks     --hack      --material
+	--panels    --pwidgets       --shades    --shapes
+	EOF
 fi

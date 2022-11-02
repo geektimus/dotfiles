c84ae4943f68eb6faa22c2bfcf34e8c23987ad42
Geektimus <alexander.kno+github@gmail.com>
2022-11-02 16:28:56 -0500

    Update qtile config

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -29,32 +29,35 @@ from .keys import mod, keys
 #         Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
 #     ])
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in [
+  "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx"
+]]
 
-for i in groups:
+for idx, group in enumerate(groups):
+    actual_key = str(idx + 1)
     keys.extend(
         [
             # mod1 + letter of group = switch to group
             Key(
                 [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                actual_key,
+                lazy.group[group.name].toscreen(),
+                desc="Switch to group {}".format(group.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             #Key(
             #    [mod, "shift"],
-            #    i.name,
-            #    lazy.window.togroup(i.name, switch_group=True),
-            #    desc="Switch to & move focused window to group {}".format(i.name),
+            #    actual_key,
+            #    lazy.window.togroup(group.name, switch_group=True),
+            #    desc="Switch to & move focused window to group {}".format(group.name),
             #),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
             Key(
                 [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name),
-                desc="move focused window to group {}".format(i.name)
+                actual_key,
+                lazy.window.togroup(group.name),
+                desc="move focused window to group {}".format(group.name)
             ),
         ]
     )
